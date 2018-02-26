c6113c993aebe48dca0974aabbbce21fca46f4f5
Daryya Buchok <daria_buchok@epam.com>
2018-02-26 15:33:47 +0300

    lab-08-done

--- src/main/java/lab/service/CountryServiceImpl.java
+++ src/main/java/lab/service/CountryServiceImpl.java
@@ -5,11 +5,13 @@ import lab.model.Country;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 //@Repository is more convenient declaration for such a class than general @Service
 @Repository
+@Transactional
 @AllArgsConstructor
 public class CountryServiceImpl implements CountryService {
 
@@ -34,26 +36,32 @@ public class CountryServiceImpl implements CountryService {
 		}
 	}
 
+	@Transactional(propagation = Propagation.REQUIRED)
 	public List<Country> getAllCountriesRequired() {
 		return jdbcCountryDao.getCountries();
 	}
 
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public List<Country> getAllCountriesRequiresNew() {
 		return jdbcCountryDao.getCountries();
 	}
 
+	@Transactional(propagation = Propagation.SUPPORTS)
 	public List<Country> getAllCountriesSupports() {
 		return jdbcCountryDao.getCountries();
 	}
 
+	@Transactional(propagation = Propagation.NEVER)
 	public List<Country> getAllCountriesNever() {
 		return jdbcCountryDao.getCountries();
 	}
 
+	@Transactional(propagation = Propagation.MANDATORY)
 	public List<Country> getAllCountriesMandatory() {
 		return jdbcCountryDao.getCountries();
 	}
 
+	@Transactional(propagation = Propagation.NOT_SUPPORTED)
 	public List<Country> getAllCountriesNotSupported() {
 		return jdbcCountryDao.getCountries();
 	}
